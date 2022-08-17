f75b08c1f0beb90ae9cf40334538faca42bc94ed
isaac <idreyes@uh.edu>
2022-08-16 22:38:03 -0500

    almost finished. Need logos and fix some sectiosns

---
+++ portfolio/src/components/NavigationDots.tsx
@@ -0,0 +1,15 @@
+const NavigationDots = ({ active}:{ active: string }) => {
+    return(
+        <div className="app__navigation">
+                {['home', 'about', 'work', 'skills', 'testimonials', 'contact'].map((item: string, index) => (
+                    <a 
+                        href={`#${item}`} 
+                        key={item + index}
+                        className="app__navigation-dot"
+                        style={active === item ? {backgroundColor: '#313BAC'} : {}}
+                    />
+                ))}
+        </div>
+    )
+}
+export default NavigationDots;
